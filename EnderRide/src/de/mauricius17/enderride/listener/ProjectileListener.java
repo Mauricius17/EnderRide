cf9602a442e78f1fb10273917d98825ec1e69bfb
BuildTools <unconfigured@null.spigotmc.org>
2015-08-31 14:48:20 +0200

    Update Beta 2.3

--- EnderRide/src/de/mauricius17/enderride/listener/ProjectileListener.java
+++ EnderRide/src/de/mauricius17/enderride/listener/ProjectileListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 import de.mauricius17.enderride.enderride.EntityEnderRide;
+import de.mauricius17.enderride.utils.OnlineMode;
 import de.mauricius17.enderride.utils.Permissions;
 import de.mauricius17.enderride.utils.UUIDFetcher;
 import de.mauricius17.enderride.utils.Utils;
@@ -20,28 +21,55 @@ public class ProjectileListener implements Listener {
 		if(e.getEntity() instanceof EnderPearl && e.getEntity().getShooter() instanceof Player) {
 			Player p = (Player) e.getEntity().getShooter();
 		
-			if(p.hasPermission(Permissions.ENDERRIDEUSE.getPermission()) && Utils.getEnderRide().contains(UUIDFetcher.getUUID(p.getName()))) {
-				if(Utils.getConfig().getBoolean("certain_worlds")) {
-					String[] worlds = Utils.getConfig().getString("worlds").split(",");
-					
-					for(String w : worlds) {
-						if(w.equals(p.getWorld().getName())) {
-							EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
-							
-							ride.teleportTo(e.getEntity().getLocation(), true);
-							ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
-							e.getEntity().remove();
-							ride.getBukkitEntity().setPassenger(p);	
-							return;
+			if(Utils.getOnlineMode().equals(OnlineMode.ON)) {
+				if(p.hasPermission(Permissions.ENDERRIDEUSE.getPermission()) && Utils.getEnderRide().contains(UUIDFetcher.getUUID(p.getName()))) {
+					if(Utils.getConfig().getBoolean("certain_worlds")) {
+						String[] worlds = Utils.getConfig().getString("worlds").split(",");
+						
+						for(String w : worlds) {
+							if(w.equals(p.getWorld().getName())) {
+								EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
+								
+								ride.teleportTo(e.getEntity().getLocation(), true);
+								ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
+								e.getEntity().remove();
+								ride.getBukkitEntity().setPassenger(p);	
+								return;
+							}
 						}
+					} else {
+						EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
+						
+						ride.teleportTo(e.getEntity().getLocation(), true);
+						ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
+						e.getEntity().remove();
+						ride.getBukkitEntity().setPassenger(p);	
 					}
-				} else {
-					EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
-					
-					ride.teleportTo(e.getEntity().getLocation(), true);
-					ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
-					e.getEntity().remove();
-					ride.getBukkitEntity().setPassenger(p);	
+				}	
+			} else {
+				if(p.hasPermission(Permissions.ENDERRIDEUSE.getPermission()) && Utils.getEnderRide().contains(p.getUniqueId())) {
+					if(Utils.getConfig().getBoolean("certain_worlds")) {
+						String[] worlds = Utils.getConfig().getString("worlds").split(",");
+						
+						for(String w : worlds) {
+							if(w.equals(p.getWorld().getName())) {
+								EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
+								
+								ride.teleportTo(e.getEntity().getLocation(), true);
+								ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
+								e.getEntity().remove();
+								ride.getBukkitEntity().setPassenger(p);	
+								return;
+							}
+						}
+					} else {
+						EntityEnderRide ride = new EntityEnderRide(((CraftWorld)Bukkit.getWorld(p.getWorld().getName())).getHandle());
+						
+						ride.teleportTo(e.getEntity().getLocation(), true);
+						ride.getBukkitEntity().setVelocity(e.getEntity().getVelocity());
+						e.getEntity().remove();
+						ride.getBukkitEntity().setPassenger(p);	
+					}
 				}
 			}
 		}
